c79785fca0b0d03f799483ae49cd7f7afbf85c9f
Annie <annie-d@hotmail.ca>
2016-04-15 17:54:01 +0000

    exercise1: gave a more specific array variable name and removed useless parameters in the function

--- answers.js
+++ answers.js
@@ -2,11 +2,11 @@
 // Write a function called printPositives that takes an array and uses its forEach method to print 
 // only the positive numbers.
 
-var negativesPositivesArray = [-3, 21, 17, -15, 43, -28];
-function printPositives(element, index, array) {
+var negativesAndPositivesArray = [-3, 21, 17, -15, 43, -28];
+function printPositives(element) {
     if (element > -1) {
         console.log(element);
     }
 }
 
-negativesPositivesArray.forEach(printPositives);
+negativesAndPositivesArray.forEach(printPositives);
